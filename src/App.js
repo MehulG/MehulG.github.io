70e6151f3481b0b33188413f9ddff2b8fc1ab3aa
MehulG <mehulgaidhani@gmail.com>
2021-03-22 00:30:46 +0530

    data changed, \n problem fixed in section

--- src/App.js
+++ src/App.js
@@ -3,16 +3,14 @@ import 'bulma';
 import * as data from "./Data/data.json";
 
 import Banner from "./component/Banner";
-// import Navbar from "./component/Navbar";
 import Section from "./component/Section";
 
 function App() {
   return (
     <div>
-      {/* <Navbar/> */}
       <Banner/>
       <div class = "container notification is-white">
-        <Section prop = {data.intro}/>
+        {data.intro.map(data => <Section prop = {data}/>)}
       </div>
     </div>
   );
--- src/component/Section.jsx
+++ src/component/Section.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import "bulma";
 
 class Section extends React.Component {
+
+  newLineText(props) {
+    const text = props.text;
+    return text.split('\n').map(str => <p>{str}</p>);
+  }
+
+
   render() {
     return (
       <div>
         <section class="section">
-          <h1 class="title">{this.props.prop.title}</h1>
-          <h2 class="subtitle">{this.props.prop.content}</h2>
+          <h1 class="title pb-4">{this.props.prop.title}</h1>
+          <h2 class="subtitle has-text-weight-light is-size-5"><this.newLineText text = {this.props.prop.content}/></h2>
         </section>
       </div>
     );
