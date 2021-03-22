0e32478765328d7214f1483ac77c84a6e7cd35fb
MehulG <mehulgaidhani@gmail.com>
2021-03-22 21:42:58 +0530

    Where I have worked

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import * as data from "./Data/data.json";
 
 import Banner from "./component/Banner";
 import Section from "./component/Section";
+import ImgSection from "./component/ImgSection";
 
 function App() {
   return (
     <div>
       <Banner/>
       <div class = "container notification is-white">
-        {data.intro.map(data => <Section prop = {data}/>)}
+        {data.intro.map(data => 
+          data.type === "text" ? <Section prop = {data}/> : <ImgSection prop = {data}/>)}
       </div>
     </div>
   );
--- src/component/ImgSection.jsx
+++ src/component/ImgSection.jsx
@@ -6,16 +6,23 @@ class ImgSection extends React.Component {
 
   placeItem(props){
     return(
-      <div class="level-item has-text-centered">
-        <div>
-          <a href= {props.prop.link} target = "_blank">
-          <figure class="image is-96x96">
-            <img src = {props.prop.url} alt = {props.prop.text}/>
-            {/* <figcaption>{props.prop.text}</figcaption> */}
-          </figure>
-          </a>
-        </div>
+      <a href={props.prop.link} target = "_blank" rel="noreferrer" class = "text-decor-none">
+        <div class="card m-2 ">
+          <div class="card-content">
+            <div class="media">
+              <div class="media-left">
+                <figure class="image is-96x96">
+                  <img src={props.prop.url} alt={props.prop.text}/>
+                </figure>
+              </div>
+              <div class="media-content">
+                <p class="title is-6">{props.prop.text}</p>
+                <p class="subtitle is-size-7">{props.prop.position}</p>
+              </div>
+            </div>
+          </div>
       </div>
+     </a>
     );
   }
 
@@ -25,7 +32,7 @@ class ImgSection extends React.Component {
         <section class="section">
           <h1 class="title pb-4">{this.props.prop.title}</h1>
           <h2 class="subtitle has-text-weight-light is-size-5">
-            <div class="flex level">
+            <div class="flex">
               {this.props.prop.content.map(data => <this.placeItem prop = {data}/>)}
             </div>
           </h2>
